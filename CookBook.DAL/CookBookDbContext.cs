87fba747168ac063a4f2f9709e6d34456fd2c7fd
Hana Hrustić <hana.hrustic@gmail.com>
2022-06-13 21:42:50 +0200

    Added appuser and migration

--- CookBook.DAL/CookBookDbContext.cs
+++ CookBook.DAL/CookBookDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using CookBook.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace CookBook.DAL
 {
-    public class CookBookDbContext : DbContext
+    public class CookBookDbContext : IdentityDbContext<AppUser>
     {
         protected CookBookDbContext() { }
         public CookBookDbContext(DbContextOptions<CookBookDbContext> options) : base(options){
